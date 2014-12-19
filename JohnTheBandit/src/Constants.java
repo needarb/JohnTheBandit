c22c087cc1ceadd524bd8350c262566801f95cbc
Ned Read <needarb@gmail.com>
2014-12-19 01:02:02 -0600

    Music

--- JohnTheBandit/src/Constants.java
+++ JohnTheBandit/src/Constants.java
@@ -3,7 +3,11 @@
  */
 public class Constants
 {
-    public static final String IMAGE_PATH = "res/images/";
+    public static final String IMAGES_PATH = "res/images/";
     public static final String CHARACTERS_IMAGE_PATH = "res/images/chars/";
     public static final String TILES_IMAGE_PATH = "res/images/tiles";
+
+    public static final String SOUNDS_PATH = "res/sounds/";
+    public static final String MUSIC_PATH = "res/sounds/music/";
+    public static final String SOUND_EFFECTS_PATH = "res/sounds/sound_effects/";
 }
--- JohnTheBandit/src/MainWindow.java
+++ JohnTheBandit/src/MainWindow.java
@@ -1,5 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 
 /**
  * Created by needa_000 on 12/19/2014.
@@ -12,7 +20,31 @@ public class MainWindow extends JFrame
         JLabel label = new JLabel("It's working!");
         label.setFont(new Font("Times New Roman",Font.BOLD,48));
         add(label);
+        startMusic();
         pack();
         setVisible(true);
+    }
+
+    private void startMusic()
+    {
+        String path = "JohnTheBandit/" + Constants.MUSIC_PATH + "a_song.mp3";
+        File file = new File(path);
+        FileInputStream fis = null;
+        try
+        {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            Player player = new Player(fis);
+            player.play();
+        } catch (JavaLayerException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
