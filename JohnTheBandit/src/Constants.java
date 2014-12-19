ac96188322b1663ec1eec61ac0acbd500317ea80
Ned Read <needarb@gmail.com>
2014-12-19 00:22:17 -0600

    File structure changes

---
+++ JohnTheBandit/src/Constants.java
@@ -0,0 +1,9 @@
+/**
+ * Created by needa_000 on 12/19/2014.
+ */
+public class Constants
+{
+    public static final String IMAGE_PATH = "res/images/";
+    public static final String CHARACTERS_IMAGE_PATH = "res/images/chars/";
+    public static final String TILES_IMAGE_PATH = "res/images/tiles";
+}
